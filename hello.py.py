a9fef1583aadc33faeeaa1b78f80131217fa1970
Minecraft200910 <97389865+Minecraft200910@users.noreply.github.com>
2022-01-21 09:48:57 +0800

    Add files via upload

---
+++ hello.py.py
@@ -0,0 +1,9 @@
+"""
+这是一个自述文件
+"""
+
+print("Hi,I'm @MINECRAFT200910")
+print("I'm intersted Java,Python ...")
+print("I,m currently learning Java.")
+print("I'm looking to collaborate.")
+print("How to reach me:Aronliang@outlook.com")
